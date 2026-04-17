175d41fcfb12209421815f2e84c8e0d72a277a11
Tony Jefferson <tonyjeffferson@outlook.com>
2026-04-17 17:50:06 -0300

    Bringup Lunaris
    Change-Id: Ice468eb325eca4312ac7622c4be2fbddb9b94b65

--- lineage_vermeer.mk
+++ lineage_vermeer.mk
@@ -14,11 +14,31 @@ $(call inherit-product, device/xiaomi/vermeer/device.mk)
 # Inherit from common lineage configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Lunaris Flags
+TARGET_ENABLE_BLUR := true
+WITH_GMS := true
+TARGET_USES_CORE_GAPPS := true
+TARGET_CUSTOM_UDFPS := true
+WITH_BCR := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_OPTIMIZED_DEXOPT := true
+PRODUCT_NO_CAMERA := false
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+TARGET_DISABLE_MATLOG := true
+
 PRODUCT_DEVICE := vermeer
 PRODUCT_NAME := lineage_vermeer
 PRODUCT_BRAND := POCO
-PRODUCT_MODEL := 23113RKC6G
+PRODUCT_MODEL := POCO F6 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.product.brand=POCO \
+    ro.product.model=POCO F6 Pro \
+    ro.product.device=vermeer \
+    ro.product.name=lineage_vermeer \
+    ro.product.manufacturer=Xiaomi \
+    net.bt.name=POCO F6 Pro
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
